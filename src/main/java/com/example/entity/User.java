b0ca016f814525160f79ebdfaec394396ed74e94
Prasantaclifav <prasanta.patra@clifav.com>
2024-06-26 15:47:42 +0530

    Scholl-Management-System

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+ private Long id;
+ private String name;
+ private String email;
+ 
+ public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+private String password;
+	
+	
+}
